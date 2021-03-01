7f6a9cae1cad410a0837762817f68dc98b4e23ab
“WesleyMeneghini” <wesley.meneghini@outlook.com>
2021-03-01 19:32:21 -0300

    Utilizando pacote para o chromedrive

--- src/config/webBrowser.py
+++ src/config/webBrowser.py
@@ -1,10 +1,13 @@
 from selenium import webdriver
-import os
+from webdriver_manager.chrome import ChromeDriverManager
 
 def browser():
-    chromedriver = r"src/config/chromedriver"
+    chromedriver = r"./src/config/chromedriver"
     # chromedriver = f'{os.getcwd()}/src/config/chromedriver'
+    # print(chromedriver)
+    # options = webdriver.ChromeOptions()
     options = webdriver.ChromeOptions()
     options.headless = True
-
-    return webdriver.Chrome(executable_path=chromedriver, options=options)
+    # return webdriver.Chrome(executable_path=chromedriver, options=options)
+    # return webdriver.Firefox(options=options)
+    return webdriver.Chrome(ChromeDriverManager().install(), options=options)
